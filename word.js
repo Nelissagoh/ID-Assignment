a403d6bdbdb8acb247c30d4368e9eb97430270e4
daphneyap <s10243748@connect.np.edu.sg>
2023-02-06 10:47:43 +0800

    game page

---
+++ word.js
@@ -0,0 +1,66 @@
+const wordList = [
+    {
+        word: "javascript",
+        hint: "programming language"
+    } ,
+
+    {
+        word: "cello",
+        hint: "musical instrument"
+    } ,
+
+    {
+        word: "pluto",
+        hint: "planet"
+    } ,
+
+    {
+        word: "aluminium",
+        hint: "metal"
+    } ,
+
+    {
+        word: "nitrogen",
+        hint: "type of gas"
+    } ,
+
+    {
+        word: "strawberry",
+        hint: "fruit"
+    } ,
+
+    {
+        word: "barney",
+        hint: "character of a children tv show"
+    } ,
+
+    {
+        word: "bitcoin",
+        hint: "type of currency"
+    } ,
+
+    {
+        word: "qatar",
+        hint: "country"
+    } ,
+
+    {
+        word: "afrikaans",
+        hint: "language"
+    } ,
+
+    {
+        word: "einstein",
+        hint: "scientist"
+    } ,
+
+    {
+        word: "disneyland",
+        hint: "amusement park"
+    } ,
+
+    {
+        word: "hockey",
+        hint: "sport"
+    }
+]
